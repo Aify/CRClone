90d6e603060d4280dc0d9bd3a2297e3c4fd1011a
TheEguy <eddiewang2002@gmail.com>
2017-05-12 19:58:07 -0700

    new classes

--- Bomber.java
+++ Bomber.java
@@ -6,5 +6,30 @@
  */
 public class Bomber extends Card 
 {
+    private int elixirCost = 3;
+    private int amountOfTroops = 1;
+    private int troopID;
+    private int spawnTime = 1;
     
+    private int id = 5;
+    private int hp = 311;
+    private int damage = 271;
+    private int range = 80;
+    private int attackCooldown = 1;
+    private int splashRange = 10;
+    
+    private Speed speed = Speed.FAST;
+    
+    private DamageType dType;
+    private TargetType tType = TargetType.GROUND;
+    private TargetType MTType = TargetType.ALL;
+    
+    private int attackTime;
+    
+    public void spawn(){
+        
+    }
+    public String toString(){
+        return "" + troopID + "[" + amountOfTroops + "[" + elixirCost + "]";
+    }
 }
