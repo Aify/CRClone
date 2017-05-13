d87d4eb813dafb8874415c2ca2187039f7e2ef3a
dabomb666666 <ericchen3339@gmail.com>
2017-05-12 20:21:59 -0700

    tesla

---
+++ Tesla.java
@@ -0,0 +1,37 @@
+/**
+ * Write a description of class Card here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tesla extends Card  
+{
+    private int elixerCost = 4;
+    private int amountOfTroops = 2;
+    private int troopID;
+    private int spawnTime = 1;
+
+
+	private int id = 1;
+    private int hp = 954;
+    private int damage = 135;
+    private int range = 110;
+    private int attackCooldown = 1;
+    private int splashRange = 0;
+    
+    private Speed speed = Speed.SLOW;
+    
+    private DamageType dType;
+    private TargetType tType = TargetType.GROUND;   // the type of the troop, eg: this is a building or this is a flying or ground
+    private TargetType mTType = TargetType.ALL;  // my t type, what the troop prefers to target
+
+    private int attackTime;
+
+    public void spawn() {
+
+    }
+    
+    public String toString() {
+        return "" + troopID + "[" + amountOfTroops + "[" + elixerCost + "]";
+    }
+}
