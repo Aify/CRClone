a7f657bac6d842f8d43e6fde62a968105667682e
Aify <hlo1453@gmail.com>
2017-05-15 23:51:14 -0700

    network class base done

--- Network.java
+++ Network.java
@@ -1,11 +1,64 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  * Write a description of class Network here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Network  
+public class Network extends Thread
 {
-    
-    
+	public Socket connection;
+
+	public PrintStream outStream;
+	public Scanner inStream;
+
+	public boolean gameStarted = false;
+
+	public Date syncTimer;
+
+	@Override
+	public void run() {
+		// connect to server
+		// TODO: CONNECT TO SERVER
+		
+		try {
+			connection = new Socket("70.79.124.240", 6789);
+			outStream = new PrintStream(connection.getOutputStream());
+			inStream = new Scanner(connection.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		// write a sync message
+		outStream.println("3{SYNC");
+		syncTimer = new Date();
+
+		while (true) {
+			if (!gameStarted) {
+				if (new Date().getTime() - syncTimer.getTime() > 15000) {
+					syncTimer = new Date();
+					outStream.println("3{SYNC");
+				} 
+			} else {
+				// game loop
+				// read messages and update gamestate
+				// note that writing messages is handled from the user input end. 
+			}
+		}
+	}
+
+	public  synchronized void writeMsg(String s) {
+		outStream.println(s);
+	}
+
+	public synchronized String readMessage() {
+		return inStream.nextLine();
+	}
 }
