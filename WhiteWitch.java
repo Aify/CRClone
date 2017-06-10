d9f2286b3c7f5b24f6270d9fe82ef25c78dac3ca
Aify <hlo1453@gmail.com>
2017-06-09 18:50:22 -0700

    whitewitch fix

--- WhiteWitch.java
+++ WhiteWitch.java
@@ -11,22 +11,6 @@ public class WhiteWitch extends Card
 		elixirCost = 7;
 		amountOfTroops = 1;
 		spawnTime = 10;
-
-
-<<<<<<< HEAD
-    private int id = 3;
-    private int hp = 220;
-    private int damage = 320;
-    private int range = 0;
-    private int attackCooldown = 7;
-    private int splashRange = 50;
-    
-    private Speed speed = Speed.MEDIUM;
-    
-    private DamageType dType;
-    private TargetType tType = TargetType.GROUND;   // the type of the troop, eg: this is a building or this is a flying or ground
-    private TargetType mTType = TargetType.ALL;  // my t type, what the troop prefers to target
-=======
 		id = 62;
 		hp = 220;
 		damage = 320;
@@ -39,7 +23,6 @@ public class WhiteWitch extends Card
 		tType = TargetType.GROUND;   // the type of the troop, eg: this is a building or this is a flying or ground
 		MTType = TargetType.ALL;  // my t type, what the troop prefers to target
 	}
->>>>>>> e9facda71d83a70bbe9508f69bb4c56ed140322c
 
 
     public void spawn() {
